2075eb315b5fbb8eea68467a0468e091ace495a4
casriel. <adlerriggstablet@gmail.com>
2026-05-12 10:45:21 -0700

    Update n3ds_renderer.c

--- src/n3ds/n3ds_renderer.c
+++ src/n3ds/n3ds_renderer.c
@@ -1,1 +1,15 @@
+#include "n3ds_renderer.h"
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <sys/stat.h>
+
+#include <3ds.h>
+#include <citro2d.h>
+#include <citro3d.h>
+
+#include "utils.h"
+#include "text_utils.h"
+#include "matrix_math.h"
