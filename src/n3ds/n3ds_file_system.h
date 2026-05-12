24e6e885d0732256763d7a710e075eb6b9474206
casriel. <adlerriggstablet@gmail.com>
2026-05-12 10:44:15 -0700

    does so much

--- src/n3ds/n3ds_file_system.h
+++ src/n3ds/n3ds_file_system.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#include "file_system.h"
+
+typedef struct {
+    FileSystem base;
+    char* basePath; // the directory containing data.win, with trailing separator
+} N3DSFileSystem;
+
+// filesytem to the data.win path blah blah
+N3DSFileSystem* N3DSFileSystem_create(const char* dataWinPath);
+void N3DSFileSystem_destroy(N3DSFileSystem* fs);
